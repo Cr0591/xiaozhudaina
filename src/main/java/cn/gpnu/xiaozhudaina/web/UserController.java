45edba1aa78c06a015f60e42b6150f1b5a875e15
Cr0591 <1275319462@qq.com>
2019-12-11 21:07:26 +0800

    修改设置信息所在位置

--- src/main/java/cn/gpnu/xiaozhudaina/web/UserController.java
+++ src/main/java/cn/gpnu/xiaozhudaina/web/UserController.java
@@ -8,10 +8,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -52,11 +56,29 @@ public class UserController {
         try{
             Integer userId = ((User)currentUser).getUserId();
             User user = userService.getUserById(userId);
+            user.setPassword("");
             modelMap.put("success",true);
             modelMap.put("user",user);
         }catch (Exception e){
             modelMap.put("success",false);
             modelMap.put("errMsg",e.getMessage());
+        }
+        return modelMap;
+    }
+
+    @RequestMapping(value = "/test",method = RequestMethod.POST)
+    @ResponseBody
+    public Map<String,Object> test(HttpServletRequest request){
+        Map<String,Object> modelMap = new HashMap<String,Object> ();
+        CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver(request.getSession().getServletContext());
+        if (multipartResolver.isMultipart(request)){
+            MultipartHttpServletRequest multipartRequest = (MultipartHttpServletRequest) request;
+            List<MultipartFile> file = multipartRequest.getFiles("file");
+            try {
+
+            }catch (Exception e){
+
+            }
         }
         return modelMap;
     }
